1e644209df39a2d2b204eab1050d00ad76acbbcb
Diedrich Vorberg <diedrich@tux4web.de>
2014-10-06 17:25:35 +0200

    Worked on.

--- t4/psg/drawing/engine_two/processors/old_style_markup.py
+++ t4/psg/drawing/engine_two/processors/old_style_markup.py
@@ -38,7 +38,7 @@ __Syntax__
       ignored. This is important for ‘pretty’ source formatting of lists.
 • All white space is normalized to one space like in HTML or LaTeX.
 • There are limited (wikklytext inspired) inline maarkup options:
-  ''bold'', //italic//, ''//bold italic//''
+  ''bold'', //italic//, ''//bold italic//'' and @@highlighted@@.
 • Headings are lines that have equal numbers of leading and trailing
   underscores. _h1_, __h2__, ___h3___.
 • Lists are paragraphs (blocks of text separated by empty lines) whose
@@ -65,13 +65,14 @@ over harbor and city
 on silent haunches
 and then moves on.
 
-
 """
 
 import re, types
 from string import *
 
 from t4.web.typography import normalize_whitespace
+
+from t4.psg.drawing.engine_two import elements
 
 # Line feeds followed by regular white-space are considered simple spaces.
 ignorable_linefeeds = re.compile(ur"\n[ \t]+")
@@ -82,7 +83,7 @@ heading_re = re.compile(ur"((?:^|\n)(_+)(.*?)(_+)(?:\n|$))")
 # Blocks are separated by more than one consecutive \n.
 block_separator_re = re.compile(ur"\n\n+")
 
-def convert(source):
+def convert(source, styles):
     """
     Convert `source`, a unicode string formatted as describe in the module
     docstring, to a engine_two.model-tree that can be rendered to PostScript
@@ -104,25 +105,31 @@ def convert(source):
     # lists and regular paragraphs.
     block_source = block_separator_re.split(source)
 
-    blocks = map(_block.from_source, block_source)
+    # Remove empty paragraphs
+    block_source = filter(lambda s: strip(s) != "", block_source)
+    
+    blocks = map(lambda source: _block.from_source(styles, source),
+                 block_source)
 
-    for b in blocks:
-        print repr(b)
+    return elements.document(styles["document"],
+                             map(lambda block: block.box(), blocks))
 
 class _block(object):
     """
     A ‘block’ in terms of the old style markup processor is a heading, a
     list or a regular paragraph. Each are rendered into specific
-    engine_two.model objects.
+    engine_two.elements.box objects.
     """
-    def __init__(self, parts):
+    def __init__(self, styles, parts):
         """
         ‘Parts’ of a paragraph are separated by soft new-lines.
         """
-        raise NotImplemented()
+        self.styles = styles
+        self.parts = parts
+        
         
     @classmethod
-    def from_source(cls, source):
+    def from_source(cls, styles, source):
         """
         Return a heading, list or paragraph object, depending on the input.
         """
@@ -131,7 +138,7 @@ class _block(object):
             # A heading.
             all, leading, text, trailing, = match.groups()
             level = len(leading)
-            return heading(text, level)
+            return heading(styles, text, level)
         else:
             parts = split(source, "\n")
 
@@ -142,35 +149,126 @@ class _block(object):
                     break
 
             if is_list:
-                return bullet_list(parts)
+                return bullet_list(styles, parts)
             else:
-                return paragraph(parts)
+                return paragraph(styles, parts)
 
     def __repr__(self):
         return "<%s with %i parts, '%s…'>" % ( self.__class__.__name__,
                                                len(self.parts),
                                                self.parts[0][:10], )
+
+    @property
+    def style(self):
+        return self.styles.get(self.__class__.__name__, None)
+
+
+    def box(self):
+        return elements.box(map(self.paragraph, self.parts), self.style)
+
         
-            
+    inline_markup_re = re.compile(r"""(''[^']+''|       # bold
+                                       //[^/]+//|       # italic
+                                       ''//[^/']+//''|  # bold-italic
+                                       @@[^@]+@@)        # highlighted
+                                    """, re.VERBOSE)
+    def paragraph(self, source):
+        texts = []
+
+        pieces = self.inline_markup_re.split(source)
+        pieces.reverse()
+
+        while pieces:
+            not_formated = pieces.pop()
+            if not_formated:
+                texts.append(elements.text(self.words(not_formated)))
+
+            if pieces:
+                formated = pieces.pop()
+                
+                if formated.startswith("''//"):
+                    text = formated[4:-4]
+                    style = self.styles["bold-italic"]
+                else:
+                    fmt = formated[:2]
+                    text = formated[2:-2]
+
+                    if fmt == "''":
+                        style = self.styles["bold"]
+                    elif fmt == "//":
+                        style = self.styles["italic"]
+                    if fmt == "@@":
+                        style = self.styles["highlighted"]
+
+                texts.append(elements.text(self.words(text, style)))
+        
+        return elements.paragraph(texts, style=self.style)
+
+    def words(self, text, style=None):
+        for word in splitfields(text):
+            yield elements.word(self.syllables(word, style), style)
+        
+    def syllables(self, word, style=None):
+        parts = split(word, elements.syllable.soft_hyphen_character)
+        for part in parts[:-1]:
+            yield elements.syllable(word, style, True)
+        yield elements.syllable(parts[-1], style, False)
+                
+                
 class heading(_block):
-    def __init__(self, text, level):
-        self.text = normalize_whitespace(text)
+    def __init__(self, styles, text, level):
+        _block.__init__(self, styles, [text,])
         self.level = level
 
     def __repr__(self):
         return "<%s '%s', level=%i>" % ( self.__class__.__name__,
                                          self.text, self.level, )
         
+    @property
+    def style(self):
+        return self.styles.get("h%i" % self.level, None)
+            
 class paragraph(_block):    
-    def __init__(self, parts):
-        self.parts = map(normalize_whitespace, parts)
+    pass
     
 class bullet_list(paragraph):
-    def __init__(self, parts):
+    def __init__(self, styles, parts):
         # Remove the bullet character from the parts.
         parts = map(lambda s:s[1:], parts)
-        paragraph.__init__(self, parts)
+        paragraph.__init__(self, styles, parts)
 
+        
 if __name__ == "__main__":
-    convert(__doc__)
-    
+    from t4.psg.drawing.engine_two.styles import lists
+    from t4.psg.drawing.engine_two.styles.computer_modern \
+      import cmu_sans_serif as cmuss, text_style, paragraph_style
+    from t4.psg.util.colors import red
+
+    document = convert(
+        __doc__, styles={ "document": cmuss,
+                          "h1": text_style({"font-size": 18,
+                                            "line-height": 22,
+                                            "font-weight": "bold"},
+                                           name="h1"),
+                          "h2": text_style({"font-size": 14,
+                                            "line-height": 18,
+                                            "font-weight": "bold"},
+                                           name="h2"),
+                          "h3": text_style({"font-size": 10,
+                                            "line-height": 14,
+                                            "font-weight": "bold"},
+                                           name="h3"),
+                          "bullet_list": paragraph_style({
+                              "list-style": lists.disk(),}, name="list"),
+                          "bold": text_style({"font-weight": "bold"},
+                                             name="bold"),
+                          "italic": text_style({"text-style": "italic"},
+                                               name="italic"),
+                          "bold-italic": text_style({"font-weight": "bold",
+                                                     "text-style": "italic"},
+                                                    name="bold-italic"),
+                          "highlighted": text_style({"font-size": 14,
+                                                     "color": red},
+                                                    name="highlighted") })
+
+    document.__print__()
