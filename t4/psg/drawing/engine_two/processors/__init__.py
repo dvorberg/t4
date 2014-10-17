a30fc3a01c6583859ee4719dc1df8d65123a7036
Diedrich Vorberg <diedrich@tux4web.de>
2014-10-17 15:24:15 +0200

    Added render_to_filename() for testing.

--- t4/psg/drawing/engine_two/processors/__init__.py
+++ t4/psg/drawing/engine_two/processors/__init__.py
@@ -29,3 +29,71 @@
 The `processors` module contains submodules, that convert representations of
 rich text into an engine_two.model-tree that can be rendered.
 """
+
+import sys, os, os.path as op
+from t4.psg.document.dsc import dsc_document
+from t4.psg.util import *
+import t4.psg.drawing.box
+    
+def render_to_filename(richtext, outfile_name):
+    """
+    Mostly for testing purposes: Render a given richtext tree on A4 paper.
+    """    
+    margin = mm(18)
+
+    outdoc = dsc_document("My first textbox example and testbed")
+
+    def next_canvas():
+        while True:
+            page = outdoc.page()
+            pcanvas = page.canvas(margin=margin)
+            
+            dist = margin / 2
+            w = pcanvas.w() / 2 - dist
+            h = pcanvas.h() / 2 - dist
+
+            canvas = t4.psg.drawing.box.canvas(pcanvas,
+                                               0, h + dist, w, h,
+                                               border=True)
+            pcanvas.append(canvas)        
+            yield canvas
+
+            canvas = t4.psg.drawing.box.canvas(pcanvas,
+                                               w + dist, h + dist, w, h,
+                                               border=True)
+            pcanvas.append(canvas)        
+            yield canvas
+
+            canvas = t4.psg.drawing.box.canvas(pcanvas,
+                                               0, 0, w, h,
+                                               border=True)
+            pcanvas.append(canvas)        
+            yield canvas
+
+            canvas = t4.psg.drawing.box.canvas(pcanvas,
+                                               w + dist, 0, w, h,
+                                               border=True)
+            pcanvas.append(canvas)        
+            yield canvas
+        
+
+    #import cProfile, pstats
+    #pr = cProfile.Profile()
+    #pr.enable()
+
+    cursor = None
+    canvases = next_canvas()
+    while True:
+        canvas = canvases.next()
+        y, cursor = richtext.render(canvas, cursor=cursor)
+        if cursor is None: break
+
+    #pr.disable()
+    #ps = pstats.Stats(pr)
+    #ps.sort_stats("tottime")
+    #ps.print_stats()
+
+    home_path = os.getenv("HOME")
+    fp = open(op.join(home_path, "Desktop", outfile_name), "w")
+    outdoc.write_to(fp)
+    fp.close()
