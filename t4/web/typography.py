1613d21165573aeb86f977652e22ec9a2639497a
Your Name <diedrich@tux4web.de>
2016-03-29 11:11:04 +0200

    Prevent single quote replacement from interfering with wikkly-stlye markup.

--- t4/web/typography.py
+++ t4/web/typography.py
@@ -40,7 +40,7 @@ def html_quote(content, lang="de"):
 opening_quote_re = re.compile(r'(\s+|^)"([0-9a-zA-Z])')
 closing_quote_re = re.compile(r'(\S+)"(\s+|$|[,\.;!])')
 opening_single_quote_re = re.compile(r"(\s+|^)'([0-9a-zA-Z])")
-closing_single_quote_re = re.compile(r"(\S+)'(\s+|$|[,\.;!])")
+closing_single_quote_re = re.compile(r"([^\s']+)'(\s+|$|[,\.;!])")
 date_until_re = re.compile(r'(\d+)\.-(\d+)\.')
 
 def improve_typography(content, lang="de"):
